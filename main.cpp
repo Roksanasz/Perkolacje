fbf17c2c25c847415a4b82ec408ffbf38f4c0e45
Roksanasz <37275319+Roksanasz@users.noreply.github.com>
2021-03-22 08:59:02 +0100

    Numery węzłów na ściankach

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <string>
 #include "union_find.h"
 #include "Perkolacja.h"
-
 #include <algorithm>
 #include <array>
 #include <cassert>
@@ -28,7 +27,50 @@ void sprawdz(UnionFind& uf)
 
 int main()
 {
-    std::vector<unsigned> side1{}, side2{}, side3{};
+    //int i;
+    int N = 10;
+    std::vector<unsigned> side1,side2,side3;
+
+    //int pionowa(int i), pozioma(int i),diag(int i);
+
+
+    /*pionowa(i)
+    {
+        return i*(i+N)/2;
+    }
+    diag(i)
+    {
+        return i*(i+3)/2;
+    }
+    pozioma(i)
+    {
+        return // tu przedział od N(N-1)/2 DO (N(N+1)/2)-1...
+    }
+    for(int i=0; i<=N;i++){
+        side1.push_back(pionowa(i));
+    }
+
+   for(int i=0; i<=N;i++){
+     side2.push_back(diag(i));
+    }
+   for(int i=0; i<=N;i++){
+     side3.push_back(pozioma(i));
+    }*/
+
+    std::vector<unsigned> pionowa ={0};
+    for(int i=0; i<=N;i++){
+        pionowa.push_back(i*(i+2)/2);
+    }
+    std::vector<unsigned> diag ={0};
+    for(int i=0; i<N;i++){
+        diag.push_back(i*(i+3)/2);
+    }
+    std::vector<unsigned> pozioma ={pionowa.back()};
+    for(int i=0; i<N;i++){
+        pozioma.push_back(pozioma.back()+i);
+    }
+
+
     Perkolacja percol{10, 0.6, side1, side2, side3};
     percol.print(std::cout);
 }
