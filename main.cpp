e25ca37eadb10fab6649385eece35018ea475c26
Roksanasz <37275319+Roksanasz@users.noreply.github.com>
2021-05-10 11:03:21 +0200

    Zapis do pliku

--- main.cpp
+++ main.cpp
@@ -11,30 +11,44 @@
 using namespace std;
 
 const double PC = 0.59274605079210;
+int policz(int N, int L)
+{
+    std::random_device rd;
+    std::mt19937 rand(rd());
+    int znalezione = 0;
+    std::ofstream plik;
+    plik.open ("dane.txt", std::fstream::app);
+    plik <<"Wyniki dla N= "<< N <<" oraz L= "<< L << ": "<< "\n";
+    for(int i = 0; i < N; i++)
+        {
+            Perkolacja percol{L, PC, rand};
+            bool b = percol.modeluj();
+            znalezione += b;
 
+            std::cout << b << " "  << double(znalezione)/(i+1.0)<<  "\n";
+            //std::cout << "\n";
+
+    plik <<  b << " "  << double(znalezione)/(i+1.0)<<  "\n";
+        }
+
+        plik.close();
+        return 0;
+
+};
 int main()
 {
     //int i;
 
-    std::random_device rd;
-    std::mt19937 rand(rd());
+
 
 
     // funkcja od N i L, wynik do pliku
     // zrobić jakieś symulacje dla kilku różnych L, np. 8, 16, 32, 64, 128, 256
     // N rzędu 10^8
     // wyznaczyc
-    const int L = 100;
-    int znalezione = 0;
-    int N = 10000;
-    for(int i = 0; i < N; i++)
-    {
-        Perkolacja percol{L, PC, rand};
-        bool b = percol.modeluj();
-        znalezione += b;
-//        percol.print(std::cout);
-        std::cout << b << " "  << double(znalezione)/(i+1.0)<<  "\n";
-        std::cout << "\n";
-    }
-    //
+    const int L = 4;
+
+    int N = 100000000;
+
+    policz(N,L);
 }
